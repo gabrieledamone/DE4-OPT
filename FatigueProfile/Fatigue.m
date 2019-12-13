ba1d345ceb5734fc9c68aff3ea4916d81ddd06a7
gabrieledamone <48175216+gabrieledamone@users.noreply.github.com>
2019-12-13 13:35:15 +0000

    Add files via upload

--- FatigueProfile/Fatigue.m
+++ FatigueProfile/Fatigue.m
@@ -4,7 +4,7 @@ S = (1:1:500);
 Integralforce = zeros(size(S)) ;
 
 % Peak force fatigue reduction with respect to number of strokes
-force = -3.*strokes + 500;
+force = -3.*strokes + 500; % assuming initial force of arbitrary value (500 N)
 
 % Determining fatigue reduction in a stroke depending on angle of exit
 for strokes = 1:1:length(S)
@@ -29,7 +29,7 @@ grid on;
 % Place equation in upper left of graph.
 xl = xlim;
 yl = ylim;
-xt = 0.05 * (xl(2)-xl(1)) + xl(1);
-yt = 0.90 * (yl(2)-yl(1)) + yl(1);
+xt = 0 * (xl(2)-xl(1)) + xl(1);
+yt = 0.2 * (yl(2)-yl(1)) + yl(1);
 caption = sprintf('y = (%.1f) x^2 + (%.1f) x + (%.1f)', p(1), p(2), p(3));
 text(xt, yt, caption, 'FontSize', 16, 'Color', 'r', 'FontWeight', 'bold');
